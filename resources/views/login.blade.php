6fc3f3948c5a78dee6f5e61327385594a6c24f89
Mikhail at dev4traders <mikha.dev@gmail.com>
2020-12-18 21:12:57 +0100

    bnt_login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -75,7 +75,7 @@
         <!-- /.col -->
         <div class="col-xs-4">
           <input type="hidden" name="_token" value="{{ csrf_token() }}">
-          <button type="submit" class="btn btn-primary btn-block btn-flat">{{ trans('admin.login') }}</button>
+          <button type="submit" class="btn btn-primary btn-block btn-flat">{{ trans('admin.btn_login') }}</button>
         </div>
         <!-- /.col -->
       </div>
